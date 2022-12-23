6d130f501ac76359c75a36ba32ffc283591f0b58
Grzegorz Wozniak <grubysweter@gmail.com>
2022-12-23 14:52:09 +0100

    STORAGE_eX-9

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {
   ref,
   uploadBytes,
   deleteObject,
+  uploadString,
 } from "firebase/storage";
 import {
   doc,
@@ -35,13 +36,22 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const db = getFirestore(app);
 
-const addPhoto = document.getElementById("addPhoto");
+const ntfPhoto = document.getElementById("addPhoto");
 const send = document.getElementById("btn");
-const toAlbum = document.getElementById("folders");
-const thumbnailImage = document.getElementById("thumbnailImage");
+const ntfName = document.getElementById("ntfName");
 const status = document.getElementById("myStatus");
+
+const storageRef = ref(storage, `${ntfName.value}.txt`);
+
+// Raw string is the default if no format is provided
+const message = "This is my message.";
+uploadString(storageRef, message).then((snapshot) => {
+  console.log("Uploaded a raw string!");
+  console.log(snapshot);
+});
+
 addPhoto.addEventListener("change", () => {
-  const photo = addPhoto.files[0];
+  const photo = ntfPhoto.files[0];
   const reader = new FileReader();
   reader.readAsDataURL(photo);
   reader.onloadend = () => {
@@ -49,41 +59,44 @@ addPhoto.addEventListener("change", () => {
     console.log(photo.name);
   };
 
-  const storageRef = ref(storage, `${toAlbum.value}/${photo.name}`);
+const storageRef = ref(storage, 
   console.log(storageRef);
   send.addEventListener("click", () => {
     status.innerText = "Przesyłamy!";
     uploadBytes(storageRef).then(() => {
       status.innerText = "Przesłano!";
     });
-  });
+  }));
 });
 
-document.getElementById("btnList").addEventListener("click", () => {
-  const storageList = ref(storage, `${toAlbum.value}`);
-  console.log(storageList);
-  listAll(storageList).then((res) => {
-    res.items.forEach((item) => {
-      getDownloadURL(item).then((url) => {
-        const myDiv = document.createElement("div");
-        const myImg = document.createElement("img");
-        const myHeader = document.createElement("h1");
-        const myButton = document.createElement("button");
 
-        myButton.innerText = "Usuń";
-        myImg.src = url;
 
-        myButton.addEventListener("click", () => {
-          deleteObject(item).then(() => {
-            document.body.removeChild(myDiv);
-          });
-        });
-        myHeader.innerText = item.fullPath;
-        myDiv.appendChild(myImg);
-        myDiv.appendChild(myHeader);
-        myDiv.appendChild(myButton);
-        document.body.appendChild(myDiv);
-      });
-    });
-  });
-});
+
+// document.getElementById("btnList").addEventListener("click", () => {
+//   const storageList = ref(storage,
+//   console.log(storageList);
+//   listAll(storageList).then((res) => {
+//     res.items.forEach((item) => {
+//       getDownloadURL(item).then((url) => {
+//         const myDiv = document.createElement("div");
+//         const myImg = document.createElement("img");
+//         const myHeader = document.createElement("h1");
+//         const myButton = document.createElement("button");
+
+//         myButton.innerText = "Usuń";
+//         myImg.src = url;
+
+//         myButton.addEventListener("click", () => {
+//           deleteObject(item).then(() => {
+//             document.body.removeChild(myDiv);
+//           });
+//         });
+//         myHeader.innerText = item.fullPath;
+//         myDiv.appendChild(myImg);
+//         myDiv.appendChild(myHeader);
+//         myDiv.appendChild(myButton);
+//         document.body.appendChild(myDiv);
+//       });
+//     });
+//   });
+// });
